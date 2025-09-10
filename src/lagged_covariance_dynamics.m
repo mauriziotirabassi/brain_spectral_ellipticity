3f876010e58bc106f50e745082886f6bd2e8428b
Maurizio Tirabassi <maurizio.tirabassi@mail.polimi.it>
2025-09-10 11:38:58 +0200

    Lagged Covariance Dynamics

---
+++ src/lagged_covariance_dynamics.m
@@ -0,0 +1,143 @@
+clear; clc; close all
+
+rng(42);
+n = 10;
+I = eye(n);
+Sigma_w = 0.1 * I; % Uncorrelated noise
+
+% Define S
+topology = 'Star';
+S = buildS(n, topology);
+Sigma = I;
+A = (-0.5 * Sigma_w + S) / Sigma;
+
+% Define A
+% topology = 'Random';
+% S = randn(n); S = 0.5*(S - S'); % Random skew-symmetric part
+% Q = randn(n); Q = Q'*Q; % Random negative definite part
+% A = -Q + S;
+% Sigma = lyap(A, Sigma_w);
+
+ev = eig(A);
+fprintf('max real part = %.4g\n', max(real(ev)));
+
+% Network topology
+S = 0.5 * (A * Sigma - Sigma * A.');
+G = digraph(S);
+figure; h = plot(G, 'Layout','circle', 'EdgeLabel',G.Edges.Weight);
+LWidths = abs(G.Edges.Weight)/max(abs(G.Edges.Weight));
+h.LineWidth = LWidths;
+
+% Simulation parameters
+n_time = 2e3; %1e4; % Simulation time steps
+transient_length = 1e3;
+tr = 0.1; % Sampling period
+t = 0 : tr : (n_time + transient_length - 1) * tr;
+
+% SIMULATE W/STOCHASTIC INPUT
+sys = ss(A, eye(n), eye(n), zeros(n));
+w = mvnrnd(zeros(1, n), Sigma_w, length(t)); % Generate noise input
+y_stoch = lsim(sys, w, t); % Simulate LTI response to noise
+y_stoch = y_stoch(transient_length + 1:end,:); % Remove transient
+
+% COVARIANCE MATRIX
+maxLag = min(size(y_stoch, 1)/2, 2000);  % number of lags to evaluate
+lags = (0:maxLag) * tr;
+
+% Theoretical covariance
+Sigma_tau = @(tau) expm(A * tau) * Sigma;
+stds_th = sqrt(diag(Sigma));
+normMat_th = stds_th * stds_th.';
+Sigma_th = nan(n,n,numel(lags));
+for k = 1:numel(lags)
+    tau = lags(k);
+    Sigma_th(:,:,k) = Sigma_tau(tau) ./ normMat_th; % Pearson correlation
+end
+
+% Empirical covariance
+Sigma_emp0 = (y_stoch' * y_stoch) / size(y_stoch,1);
+stds_emp = sqrt(diag(Sigma_emp0));
+normMat_emp = stds_emp * stds_emp';
+Sigma_emp = nan(n,n,numel(lags));
+for k = 0:maxLag
+    X = y_stoch(1:end-k, :);
+    X_lag = y_stoch(1+k:end, :);
+    Sigma_emp(:,:,k+1) = (X_lag' * X) / (size(y_stoch, 1) - k) ./ normMat_emp;
+end
+
+% Extend to negative lags based on property C_ij(-tau) = C_ji(tau)
+lags_full = [-fliplr(lags(2:end)), lags]; % Symmetric lag vector
+Sigma_th_full = nan(n,n,numel(lags_full));
+Sigma_emp_full = nan(n,n,numel(lags_full));
+for i = 1:n
+    for j = 1:n
+        Cth_pos = squeeze(Sigma_th(i,j,:));
+        Cth_neg = squeeze(Sigma_th(j,i,2:end));
+        Sigma_th_full(i,j,:) = [flipud(Cth_neg); Cth_pos];
+
+        Cemp_pos = squeeze(Sigma_emp(i,j,:));
+        Cemp_neg = squeeze(Sigma_emp(j,i,2:end));
+        Sigma_emp_full(i,j,:) = [flipud(Cemp_neg); Cemp_pos];
+    end
+end
+
+% FCD-STYLE SIMILATIRY ACROSS LAGS
+% Seeing at what lag the dynamics are the same.
+% triuIdx = find(triu(ones(n),1));
+triuIdx = find(triu(abs(S) > 0, 1)); % Isolate only active pairs
+
+% Theoretical
+vecs_th = [];
+for k = 1:length(lags_full)
+    Ck = Sigma_th_full(:,:,k);
+    vecs_th(:,k) = Ck(triuIdx);
+end
+FCD_th = corr(vecs_th);
+
+% Empirical
+vecs_em = [];
+for k = 1:length(lags_full)
+    Ck = Sigma_emp_full(:,:,k);
+    vecs_em(:,k) = Ck(triuIdx);
+end
+FCD_emp = corr(vecs_em);
+
+% Plot
+figure, tiledlayout(1, 2, 'TileSpacing','compact','Padding','compact');
+nexttile, imagesc(lags_full, lags_full, FCD_th);
+axis square; colorbar; colormap jet;
+xlabel('Lag \tau_1'); ylabel('Lag \tau_2');
+title(sprintf('Theoretical %s', topology));
+
+nexttile, imagesc(lags_full, lags_full, FCD_emp);
+axis square; colorbar; colormap jet;
+xlabel('Lag \tau_1'); ylabel('Lag \tau_2');
+title(sprintf('Empirical %s', topology));
+
+function S = buildS(n, topology)
+%BUILD S Construct skew-symmetric adjacency matrix S
+%   n        : number of nodes
+%   topology : 'chain', 'uni_chain', 'ring', 'star'
+
+S = zeros(n);
+switch lower(topology)
+    case 'chain'  % chain
+        for i = 1:n-1
+            S(i,i+1) = 1;
+            S(i+1,i) = -1;
+        end
+    case 'ring'
+        for i = 1:n
+            j = mod(i,n) + 1;
+            S(i,j) = 1;
+            S(j,i) = -1;
+        end
+    case 'star'
+        for j = 2:n
+            S(1,j) = 1;
+            S(j,1) = -1;
+        end
+    otherwise
+        error('Unknown topology: %s', topology)
+end
+end
